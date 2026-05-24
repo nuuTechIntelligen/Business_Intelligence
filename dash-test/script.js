b977506e869f8fe92147a6d189653fc5bd0b603f
nuuTechIntelligen <nuu.tech.intelligence@gmail.com>
2026-05-24 12:28:30 -0600

    Update script.js

--- dash-test/script.js
+++ dash-test/script.js
@@ -1,5 +1,5 @@
 // Agrega el parámetro ?sheet=Base_Datos para forzar a la API a leer la pestaña correcta
-const API_URL = 'https://sheetdb.io/api/v1/2s1p744rscfly?sheet=Base_Datos'; 
+const API_URL = 'https://sheetdb.io/api/v1/pw56k97827k1u'; 
 
 let baseDatosCompleta = [];
 let clienteSeleccionado = null;
